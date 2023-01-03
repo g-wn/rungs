be51143f55caa1db5b470cfeeca8216d315ccc36
Gray Nance <graynance@gmail.com>
2023-01-03 07:48:59 -0800

    profile dropdown

--- react-app/src/components/nav/NavBar.js
+++ react-app/src/components/nav/NavBar.js
@@ -1,64 +1,66 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs';
 import { HiChatBubbleLeftEllipsis } from 'react-icons/hi2';
 import { IoHomeSharp } from 'react-icons/io5';
-import { MdPeopleAlt, MdOutlineArrowDropDown } from 'react-icons/md';
+import { MdPeopleAlt } from 'react-icons/md';
 import './Nav.css';
+import ProfileDropdown from './ProfileDropdown';
 
 const NavBar = () => {
+  const [showSearchBar, setShowSearchBar] = useState(false);
   return (
     <nav className='main-nav-bar'>
-      <div className='nav-left'>
-        <div className='rungs-main-icon'>
+      <div className='main-nav-bar-content'>
+        <div className='nav-left'>
+          <div className='rungs-main-icon'>
+            <NavLink
+              to='/feed'
+              activeClassName='rungs-main-icon-link'
+            >
+              ICON
+            </NavLink>
+          </div>
+          <div className='nav-search'>
+            <div className='nav-search-icon'>
+              <BsSearch size={20} />
+            </div>
+            <div className='nav-search-text'>Search</div>
+          </div>
           <NavLink
             to='/feed'
-            activeClassName='rungs-main-icon-link'
+            activeClassName='active'
+            className='nav-home'
           >
-            ICON
+            <div className='nav-home-icon'>
+              <IoHomeSharp size={23} />
+            </div>
+            <div className='nav-home-text'>Home</div>
+          </NavLink>
+          <NavLink
+            to='/mynetwork'
+            activeClassName='active'
+            className='nav-network'
+          >
+            <div className='nav-network-icon'>
+              <MdPeopleAlt size={25} />
+            </div>
+            <div className='nav-network-text'>My Network</div>
+          </NavLink>
+          <NavLink
+            to='/messaging'
+            activeClassName='active'
+            className='nav-messaging'
+            exact
+          >
+            <div className='nav-messaging-icon'>
+              <HiChatBubbleLeftEllipsis size={24} />
+            </div>
+            <div className='nav-messaging-text'>Messaging</div>
           </NavLink>
         </div>
-        <div className='nav-search'>
-          <div className='nav-search-icon'>
-            <BsSearch size={20} />
-          </div>
-          <div className='nav-search-text'>Search</div>
-        </div>
-        <NavLink
-          to='/feed'
-          activeClassName='active'
-          className='nav-home'
-        >
-          <div className='nav-home-icon'>
-            <IoHomeSharp size={23} />
-          </div>
-          <div className='nav-home-text'>Home</div>
-        </NavLink>
-        <NavLink
-          to='/mynetwork'
-          activeClassName='active'
-          className='nav-network'
-        >
-          <div className='nav-network-icon'>
-            <MdPeopleAlt size={25} />
-          </div>
-          <div className='nav-network-text'>My Network</div>
-        </NavLink>
-        <NavLink
-          to='/messaging'
-          activeClassName='active'
-          className='nav-messaging'
-          exact
-        >
-          <div className='nav-messaging-icon'>
-            <HiChatBubbleLeftEllipsis size={24}/>
-          </div>
-          <div className='nav-messaging-text'>Messaging</div>
-        </NavLink>
-      </div>
-      <div className='nav-right'>
-        <div className='nav-bar-drop-down'>
-          <div className="nav-bar-drop-down-icon"></div>
-          <div className="nav-bar-drop-down-text">Me <MdOutlineArrowDropDown size={20}/></div>
+        <div className='nav-right'>
+            <ProfileDropdown />
         </div>
       </div>
     </nav>
---
+++ react-app/src/components/nav/ProfileDropdown.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import { MdOutlineArrowDropDown } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+import './ProfileDropdown.css';
+
+const ProfileDropdown = () => {
+  const [showProfileDrop, setShowProfileDrop] = useState(false);
+  const currentUser = useSelector(state => state.session.user);
+
+  const openDrop = () => {
+    if (showProfileDrop) return;
+    setShowProfileDrop(true);
+  };
+
+  useEffect(() => {
+    if (!showProfileDrop) return;
+
+    const closeDrop = () => {
+      setShowProfileDrop(false);
+    };
+
+    document.addEventListener('click', closeDrop);
+
+    return () => document.removeEventListener('click', closeDrop);
+  }, [showProfileDrop]);
+
+  return (
+    <div className='main-nav-profile-drop-container'>
+      <div
+        className='nav-bar-drop-down'
+        onClick={openDrop}
+      >
+        <div className='nav-bar-drop-down-icon'></div>
+        <div className='nav-bar-drop-down-text'>
+          Me <MdOutlineArrowDropDown size={20} />
+        </div>
+      </div>
+      {showProfileDrop && (
+        <div className='main-nav-profile-drop-content'>
+          <header className='profile-drop-header'>
+            <div className='profile-drop-header-user-details'>
+              <div className='profile-drop-header-user-img'></div>
+              <div className='profile-drop-header-user'>
+                <div className='profile-drop-header-user-name'>
+                  {currentUser.firstName} {currentUser.lastName}
+                </div>
+                <div className='profile-drop-header-user-bio'>bio</div>
+              </div>
+            </div>
+            <div className='profile-drop-header-profile-btn'>
+              <NavLink to={`/users/${currentUser.id}`}>View Profile</NavLink>
+            </div>
+          </header>
+          <div className='profile-drop-btns'>
+            <LogoutButton />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfileDropdown;
