46858831c69af1b4f914d7d8ec500b7c4f4d24be
Gray Nance <graynance@gmail.com>
2023-01-08 13:44:05 -0800

    update bio

--- app/forms/profile_form.py
+++ app/forms/profile_form.py
@@ -2,7 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
+
 class ProfileForm(FlaskForm):
     profile_image_url = StringField("profile_image_url")
     banner_image_url = StringField("banner_image_url")
-    bio = TextAreaField("bio")
+    bio = TextAreaField(
+        "bio",
+        validators=[
+            DataRequired(message="Please enter some information for your bio."),
+            Length(
+                min=3,
+                max=100,
+                message="Please enter a bio between 3 and 100 characters.",
+            ),
+        ],
+    )
